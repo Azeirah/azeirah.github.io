e1fdaa9865c48ede186130beeac834707f8ba467
Martijn <Tijntje_7@msn.com>
2013-06-17 16:10:20 +0200

    First commit

---
+++ External.js
@@ -0,0 +1,42 @@
+strip = function(url){
+  url = url.split('.')[0];
+  url = url.split('/');
+  return url[2]
+}
+
+var grooveshark = function(){
+  var newtitle = 'Grooveshark - ' + $('#now-playing-metadata').text();
+	$('title').text(newtitle);
+}
+
+var pandora = function(){
+  var newtitle = 'Pandora - ' + $('.info .playerBarSong').text() + ' - ' + $('.info .playerBarArtist').text();
+	$('title').text(newtitle);
+}
+
+var plug = function(){
+  var newtitle = 'Plug.dj - ' + $('#now-playing-value').text();
+	$('title').text(newtitle);
+}
+
+redirects = {
+'grooveshark': grooveshark,
+'plug': plug,
+'pandora': pandora
+}
+
+site = strip(window.location.origin);
+var i = 1;
+function myLoop () {
+   setTimeout(function () {
+      redirects[site]();
+      i++;
+      if (i < 1e4) {
+         myLoop();
+      }
+   }, 10000)
+}
+if(site in redirects){
+	redirects[site]();
+	myLoop();
+}
