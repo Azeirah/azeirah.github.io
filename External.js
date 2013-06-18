cfb05cb3fb3cf75f3e1e05e0cc77167baef109af
Martijn <Tijntje_7@msn.com>
2013-06-19 01:29:49 +0200

    zaycev had multiple divs with current artist/song resulting in insane title

--- External.js
+++ External.js
@@ -32,7 +32,7 @@ var plug = function(){
 }
 
 var zaycev = function(){
-	var newtitle = 'Zaycev - ' + $('.ontheair_artist').text() + $('.ontheair_song').text();
+	var newtitle = 'Zaycev - ' + $('#zp_current_song .ontheair_artist').text() + $('#zp_current_song .ontheair_song').text();
 	console.log(newtitle);
 	$('title').text(newtitle);
 }
